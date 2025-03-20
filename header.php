a2ce33f746a01a3dffe55521191fdaa5d7ecb876
phanupon phasuchaisakul <phanupon.p@gmail.com>
2025-03-20 12:01:27 +0700

    commit 20
    login form

--- header.php
+++ header.php
@@ -2,10 +2,7 @@
     <head>
         <meta charset ="utf-8">
         <title> Login system</title>
-        <link href="include/bootstrap-5.3.3/dist/css/bootstrap.min.css"></link>
-        <script src="include/bootstrap-5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     </head>
     <body>
-    <div class="container">    
-    <h1>user Login system</h1>
--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<?php 
-include('header.php');
+<?php include('header.php'); ?>
+<h1>Login system </h1>
 
+<div class="container">
 
-include('footer.php');
+<form class="form" name="form 1" method="post" action="check_login.php">
+   <div class="form-group"> 
+  <label>Login name</label>
+  <input name="username" type="text" id="username">
+   </div>
 
-?>
+  <div class="form-group"> 
+  <label>password</label>
+  <input name="username" type="text" id="username">
+  </div>
+
+  <div>
+  <input type="submit" name="login" value="login" class="btn btn-success" >
+ </div>
+</form>
+</div>
+
+<?php include('footer.php'); ?>
