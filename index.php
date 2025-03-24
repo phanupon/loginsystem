c8405b069a1b7d24dfa0bd21788850a7ce328d0d
phanupon phasuchaisakul <phanupon.p@gmail.com>
2025-03-24 16:43:28 +0700

    add form class
    add class in form

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
 <form class="form" name="form 1" method="post" action="check_login.php">
    <div class="form-group"> 
   <label>Login name</label>
-  <input name="username" type="text" id="username">
+  <input class="form-control" name="username" type="text" id="username" >
    </div>
 
   <div class="form-group"> 
   <label>password</label>
-  <input name="username" type="text" id="username">
+  <input class="form-control" name="username" type="text" id="username">
   </div>
-
-  <div>
+  <div><br></div>
+  <div class="form-group">
   <input type="submit" name="login" value="login" class="btn btn-success" >
  </div>
-</form>
+ </form>
 </div>
 
 <?php include('footer.php'); ?>
